a450914aa685c91dad9181aa88107d4063a01191
Michal Koszalka <michal.koszalka@jitsolutions.pl>
2017-11-19 10:23:46 +0100

    zadanie 3 wykresy

---
+++ Zadanie3/fptTprFunction.R
@@ -0,0 +1,9 @@
+calculateTPRFPR = function(confusionMatrix){
+  cmt<- confusionMatrix$table;
+  FPR = cmt[2,1]/(cmt[2,1] + cmt[1,1]);
+  TPN = cmt[2,2]/(cmt[2,2] +cmt[1,2]);
+  x <- c(FPR, TPN);
+  return(x);
+}
+
+
